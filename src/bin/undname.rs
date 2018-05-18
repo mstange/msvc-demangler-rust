abd7b32bfc64f9b22a695a8ff45eca983e4164d6
Nathan Froyd <froydnj@gmail.com>
2018-05-18 11:32:47 -0400

    add an undname binary
    c++filt is a useful tool to have for shell pipelines and whatnot.  No
    reason we shouldn't have an MSVC-alike.

---
+++ src/bin/undname.rs
@@ -0,0 +1,38 @@
+extern crate msvc_demangler;
+
+use std::env;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let args = env::args();
+
+    let print_demangled = |sym: &str| {
+        let demangled = msvc_demangler::demangle(&sym, msvc_demangler::DemangleFlags::LotsOfWhitespace);
+        match demangled {
+            Ok(ref string) => println!("{}", string),
+            _ => println!("{}", sym),
+        }
+    };
+
+    if args.len() == 1 {
+        let stdin = io::stdin();
+        let handle = stdin.lock();
+
+        for line in handle.lines() {
+            match line {
+                Ok(line) => print_demangled(&line),
+                _ => continue,
+            }
+        }
+        return;
+    }
+
+    for (i, arg) in env::args().enumerate() {
+        if i == 0 {
+            continue;
+        }
+
+        print_demangled(&arg);
+    }
+}
